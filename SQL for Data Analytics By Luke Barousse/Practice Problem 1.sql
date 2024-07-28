50eace444a0aa54fcba00a7f045dfc5930bceed2
Sakshi Andhale <103496510+SakshiAndhale@users.noreply.github.com>
2024-07-28 23:18:24 +0530

    Add files via upload

---
+++ SQL for Data Analytics By Luke Barousse/Practice Problem 1.sql
@@ -0,0 +1,20 @@
+-- PRACTICE RPOBLEMS 
+
+/* 1. Get the unique (distinct) job locations in the job_postings_fact table. Order in alphabetical order(ascending order) 
+SELECT DISTINCT job_location
+FROM job_postings_fact
+ORDER BY 
+         job_location ASC
+*/
+
+/* 2. In the job_postings_fact table get the columns job_id, job_title_short, job_location, and job_via columns. And order it in ascending order by job_location.
+
+SELECT job_id, job_title_short, job_location, job_via 
+FROM job_postings_fact
+ORDER BY job_location ASC */ 
+
+/* 3. In the job_postings_fact table get the columns job_id, job_title_short, job_location, job_via, and salary_year_avg columns. And only look at rows where job_title_short is "Data Engineer" 
+
+SELECT job_id, job_title_short, job_location, job_via, salary_year_avg,
+FROM job_postings_fact
+WHERE job_title_short = 'Data Engineer' */

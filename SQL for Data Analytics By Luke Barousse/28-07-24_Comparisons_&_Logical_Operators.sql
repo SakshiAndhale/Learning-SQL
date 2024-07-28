16f04b560e6b096b68da6a48f42757d866b65a6b
Sakshi Andhale <103496510+SakshiAndhale@users.noreply.github.com>
2024-07-28 23:56:52 +0530

    Add files via upload

---
+++ SQL for Data Analytics By Luke Barousse/28-07-24_Comparisons_&_Logical_Operators.sql
@@ -0,0 +1,12 @@
+-- COMPARISONS & LOGICAL OPPERATORS 
+-- used within the WHERE or HAVING clause 
+-- Used in conjunction with comparison operators: =, <>, <,>,>=,<= 
+-- Used in conjunction with logical operators AND, OR, BETWEEEN and IN 
+
+-- NOT EQUAL TO OPERATOR 
+-- Symbol: <> and NOT 
+-- Example WHERE job_via <> 'via LinkedIn' 
+
+/*SELECT job_id, job_title_short, job_via, salary_year_avg
+FROM job_postings_fact
+WHERE job_via <> 'via Ai-Jobs.net'*/ 

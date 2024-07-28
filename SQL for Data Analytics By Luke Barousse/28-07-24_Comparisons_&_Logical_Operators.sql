9fe8629916f66a40b0fc0299db098f9f94a7319b
Sakshi Andhale <103496510+SakshiAndhale@users.noreply.github.com>
2024-07-28 23:58:21 +0530

    Update 28-07-24_Comparisons_&_Logical_Operators.sql

--- SQL for Data Analytics By Luke Barousse/28-07-24_Comparisons_&_Logical_Operators.sql
+++ SQL for Data Analytics By Luke Barousse/28-07-24_Comparisons_&_Logical_Operators.sql
@@ -1,4 +1,4 @@
--- COMPARISONS & LOGICAL OPPERATORS 
+-- COMPARISONS & LOGICAL OPERATORS 
 -- used within the WHERE or HAVING clause 
 -- Used in conjunction with comparison operators: =, <>, <,>,>=,<= 
 -- Used in conjunction with logical operators AND, OR, BETWEEEN and IN 
@@ -10,3 +10,6 @@
 /*SELECT job_id, job_title_short, job_via, salary_year_avg
 FROM job_postings_fact
 WHERE job_via <> 'via Ai-Jobs.net'*/ 
+
+-- GREATER THAN & GREATER THAN OR EQUAL TO OPERATOR
+

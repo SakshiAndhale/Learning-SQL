2678e6b2e9a195cd011464ed016e7975d1d7af75
Sakshi Andhale <103496510+SakshiAndhale@users.noreply.github.com>
2024-07-29 15:05:43 +0530

    Update 28-07-24_Comparisons_&_Logical_Operators.sql

--- SQL for Data Analytics By Luke Barousse/28-07-24_Comparisons_&_Logical_Operators.sql
+++ SQL for Data Analytics By Luke Barousse/28-07-24_Comparisons_&_Logical_Operators.sql
@@ -12,4 +12,55 @@ FROM job_postings_fact
 WHERE job_via <> 'via Ai-Jobs.net'*/ 
 
 -- GREATER THAN & GREATER THAN OR EQUAL TO OPERATOR
+SELECT job_id, job_title_short, job_via, salary_year_avg
+FROM job_posting_fact
+WHERE salary_year_avg > 50000
+ORDER BY salary_year_avg
+
+-- LESS THAN & LESS THAN OR EQUAL TO OPERATOR
+-- Works the same way as the above
+-- Example
+SELECT job_id, job_title_short, job_via, salary_year_avg
+FROM job_posting_fact
+WHERE salary_year_avg > 50000
+ORDER BY salary_year_avg
+
+-- AND OPERATOR
+-- Great in conditions where you need to meet multiple conditions
+-- Only shows records where all conditions are true
+-- Can have multiple AND conditions
+
+SELECT job_id, job_title_short, job_via, salary_year_avg
+FROM job_posting_fact
+WHERE salary_year_avg > 50000
+     AND job_title = 'Data Analyst'
+ORDER BY salary_year_avg
+
+-- OR OPERATOR
+-- OR only shows records where any of the conditions are true
+-- can have multiple OR conditions
+
+SELECT job_id, job_title_short, job_via, salary_year_avg
+FROM job_posting_fact
+WHERE salary_year_avg > 50000
+     OR job_title = 'Data Analyst'
+ORDER BY salary_year_avg
+
+-- BETWEEN OPERATOR
+-- Gets values within a given range
+-- an alternative to using AND, <= and >=
+
+SELECT job_id, job_title_short, job_via, salary_year_avg
+FROM job_posting_fact
+WHERE salary_year_avg BETWEEN 50000 AND 90000
+ORDER BY salary_year_avg
+
+-- IN OPERATOR
+-- Specify multiple values in a WHERE clause
+-- Alternative to using OR operator
+
+SELECT job_id, job_title_short, job_via, salary_year_avg
+FROM job_posting_fact
+WHERE job_location IN ('Boston, MA', 'Anywhere')
+
 

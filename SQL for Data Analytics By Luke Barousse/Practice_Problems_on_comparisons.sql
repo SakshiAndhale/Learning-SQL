4168b395f19ba9699bf7df5017392af8b569a608
Sakshi Andhale <103496510+SakshiAndhale@users.noreply.github.com>
2024-07-30 21:41:23 +0530

    Update Practice_Problems_on_comparisons.sql

--- SQL for Data Analytics By Luke Barousse/Practice_Problems_on_comparisons.sql
+++ SQL for Data Analytics By Luke Barousse/Practice_Problems_on_comparisons.sql
@@ -14,3 +14,27 @@
 -- SELECT job_id, job_title_short, job_location, job_via, salary_year_avg
 -- FROM job_postings_fact
 -- WHERE NOT job_schedule_type = 'Part-time'
+
+/* 4. In the job_postings_fact table get the job_id, job_title_short, job_location, job_via, and salary_year_avg columns. Only look at hobs that are not posted 'via LinkedIn'*/
+-- SELECT job_id, job_title_short, job_location, job_via, salary_year_avg
+-- FROM job_postings_fact
+-- WHERE job_via <> 'via LinkedIn'
+
+/* 5. In the job_postings_fact table get the job_id, job_title_short, job_location, job_via, and salary_year_avg columns. Only get jobs that have an average yearly salary of $65,000 or greater*/
+-- SELECT job_id, job_title_short, job_location, job_via, salary_year_avg
+-- FROM job_postings_fact
+-- WHERE salary_year_avg >= 65000
+-- ORDER BY salary_year_avg
+
+/* 6. In the job_postings_fact table get the job_id, job_title_short, job_location, job_via, and salary_year_avg columns. Only get jobs that have an average yearly salary greater than $55,000*/
+-- SELECT job_id, job_title_short, job_location, job_via, salary_year_avg
+-- FROM job_postings_fact
+-- WHERE salary_year_avg > 55000
+-- ORDER BY salary_year_avg
+
+
+/* 7. In the job_postings_fact table get the job_id, job_title_short, job_location, job_via, and salary_year_avg columns. Only get jobs that have an average yearly salary less than $110000*/
+-- SELECT job_id, job_title_short, job_location, job_via, salary_year_avg
+-- FROM job_postings_fact
+-- WHERE salary_year_avg < 110000
+-- ORDER BY salary_year_avg
